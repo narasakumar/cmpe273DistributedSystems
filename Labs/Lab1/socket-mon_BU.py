0eed9122b79a3bfd3f8e8875d18548094aba5a54
Narasa Kumar <knarasa@gmail.com>
2017-02-16 13:41:48 -0800

    finalFile-SortingBasedonProcesses

--- Labs/Lab1/socket-mon_BU.py
+++ Labs/Lab1/socket-mon_BU.py
@@ -1,9 +1,9 @@
 import psutil
 import operator
 
+print '"pid", "laddr", "raddr", "status" \n'
 
-
-
+i=0
 store={}
 
 for line in psutil.net_connections(kind='tcp'):
@@ -35,42 +35,28 @@ for line in psutil.net_connections(kind='tcp'):
 
 		raddr="%s@%s" %(raddr1,raddr2)
 		#print '"%s", "%s", "%s", "%s" \n' %(pid, laddr, raddr,status)
-		
+		store[i] = []
 
 		nu = 1
-		
-		if pid in store:
-		#if store.has_key(pid):
-  			#print pid, " FOUND"
-  			nu=nu+store[pid][0]
-  			store[pid][0]=nu
-  			store[pid].append([laddr1,laddr2, raddr1,raddr2,status])
-		else: 
-			#print pid," NOT FOUND"
-			store[pid] = []
-			store[pid].append(1)
-			store[pid].append([laddr1,laddr2, raddr1,raddr2,status])
 
-		#print type(store[pid][0])
-		#print "pid=",pid, "nu=",store[pid][0]
+		if(pid in store.values()):
+			nu=nu+store[key][6]
+		else:
+			nu=1
+
+
+		store[i].extend([pid, laddr1,laddr2, raddr1,raddr2,status,nu])
+
+		i=i+1
+
 #print  store
 
-
-print '"pid", "laddr", "raddr", "status" \n'
-
-for key,value in sorted(store.iteritems(), key=operator.itemgetter(1),reverse=True):
-#for key,value in sorted(store.iteritems(), key=lambda x:(x[0]),reverse=True):
-		#print "key",key, "value",value, "\n\n"
-	
-	#print "pid=",key, "nu=",value[0]
-
-	for i in value:
-		#for j in i[1]:
-		#	print
-		if type(i) is list:
-			print '"%d", "%s@%s", "%s@%s", "%s" \n' %(key,i[0], i[1],i[2],i[3],i[4])
-#for key,value in sorted(store.iteritems(), key=lambda x:(x[6],x[1])):
+#for key,value in sorted(store.iteritems(), key=operator.itemgetter(1)):
 #	print '"%d", "%s@%s", "%s@%s", "%s" \n' %(store[key][0], store[key][1],store[key][2],store[key][3],store[key][4], store[key][5])
+
+
+for key,value in sorted(store.iteritems(), key=lambda x:(x[6],x[1])):
+	print '"%d", "%s@%s", "%s@%s", "%s" \n' %(store[key][0], store[key][1],store[key][2],store[key][3],store[key][4], store[key][5])
 
 #[t[0] for t in sorted(d.items(), key=lambda x:(x[1],x[0]))]
 
